4c78bddcaf8038c4cf861ca7fa49fee0a6dc744a
Alin Ivanoiu <alin.ivanoiu@gmail.com>
2020-10-06 01:32:56 +0300

    main.c added

---
+++ main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Testfile
+//
+//  Created by Alin Ivanoiu on 11/09/2020.
+//  Copyright © 2020 Alin Ivanoiu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
